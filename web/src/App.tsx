b5aa3580b5e251187a4778bd27b426767bef73fc
Bruno Becoski <brunobecoski@gmail.com>
2022-05-11 23:41:59 -0300

    Atualização do componente NavBar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,8 +3,8 @@ import { Header } from './components/Header';
 import { Widget } from './components/Widget';
 
 export function App() {
-  return (
-    <div className="max-w-screen-lg mx-auto">
+ return (
+    <div>
       <NavBar />
       <Header />
       <Widget />
--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,18 +1,51 @@
-import { List } from 'phosphor-react';
+import { useEffect, useRef, useState } from 'react';
+import { FacebookLogo, InstagramLogo, List, X, YoutubeLogo } from 'phosphor-react';
 
 import logoImageUrl from '../assets/logo.svg';
+import logoOnSurfaceUrl from '../assets/logoOnSurface.svg';
 
 export function NavBar() {
+ const [offset, setOffset] = useState(0);
+ const [menuIsOpen, setMenuIsOpen] = useState(false);
+ const navRef = useRef<HTMLDivElement>(null);
+
+  function setScroll() {
+    setOffset(window.scrollY);
+  };
+  
+  useEffect(() => {
+    if(offset > 0) {
+      navRef.current?.classList.add("bg-brand-500");
+      navRef.current?.getElementsByClassName("button-list")[0].classList.add("text-brand-50");
+      
+    } else {
+      navRef.current?.classList.remove("bg-brand-500");
+      navRef.current?.getElementsByClassName("button-list")[0].classList.remove("text-brand-50");
+    }
+  }, [offset])
+
+  useEffect(() => {
+    window.addEventListener("scroll", setScroll);
+    return () => {
+      window.removeEventListener("scroll", setScroll);
+    };
+  }, []);
+
   return (
-    <nav 
-      className="flex justify-between items-center w-screen h-16 fixed px-6 py-4"
+    <nav
+      ref={navRef} 
+      className="flex justify-between items-center w-full fixed px-6 py-4"
     >
       <a href="#">
-        <img src={logoImageUrl} alt="Logo doctor care" />
+        <img src={offset > 0 ? logoOnSurfaceUrl : logoImageUrl } alt="Logo DoctorCare" />
       </a>
 
-      <button className="text-brand-500">
-        <List size={20} weight={'bold'} />
+      <button onClick={() => setMenuIsOpen(!menuIsOpen)}className="button-list text-brand-500">
+        {
+          menuIsOpen
+          ? <X size={20} weight={'bold'} />
+          : <List size={20} weight={'bold'} />
+        }
       </button>
     </nav>
   );
