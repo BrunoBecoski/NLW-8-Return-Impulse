7d2d7440bba77d657af00e66a2881d4ccb856d56
Bruno Becoski <brunobecoski@gmail.com>
2022-05-19 23:58:30 -0300

    Atualização do componente Depositions

--- web/src/components/Depositions.tsx
+++ web/src/components/Depositions.tsx
@@ -1,4 +1,5 @@
 import { Quotes } from "phosphor-react";
+import { useRef } from "react";
 
 interface CardProps {
   text: string;
@@ -7,7 +8,7 @@ interface CardProps {
 
 function Card({ text, client }: CardProps) {
   return(
-    <div className="p-8 flex flex-col gap-5 bg-brand-100 ">
+    <div className="p-8 flex flex-col gap-5 bg-brand-100 w-[calc(100vw_-_3rem)]">
       <Quotes className="text-brand-500" weight="fill" size={40} />
       <p>{text}</p>
       <div className="flex items-center gap-4 ">
@@ -23,6 +24,16 @@ function Card({ text, client }: CardProps) {
 }
 
 export function Depositions() {
+  const carouselRef = useRef<HTMLDivElement>({} as HTMLDivElement);
+
+  function click1() {
+    carouselRef.current.scrollLeft -= (carouselRef.current.offsetWidth + 70);
+  }
+  
+  function click2() {
+    carouselRef.current.scrollLeft += (carouselRef.current.offsetWidth + 70);
+  }
+   
   return (
     <section className="px-6 py-24 bg-brand-50 rounded"> 
       <header className="mb-14">
@@ -30,22 +41,22 @@ export function Depositions() {
         <h2 className="font-bold text-3xl text-brand-headline">O que os clientes dizem sobre o DoctorCare</h2>
       </header>
       
-      <div className="relative overflow-hidden h-96">
-        <div className="flex gap-8 absolute">
+      <div ref={carouselRef}  className="flex items-center relative overflow-hidden scroll-smooth">
+        <div className="flex items-center gap-10">
           <Card 
-            text="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim." 
+            text="UM Velit officia consequat duis enim. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint." 
             client="Bruno Becoski"
           />
           <Card
-            text="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim." 
+            text="DOIS Velit officia consequat duis enim. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint." 
             client="Bruno Becoski"
           />
         </div>
       </div>
 
-      <div className="flex gap-2 justify-center">
-        <button className="h-3 w-3 bg-brand-500 rounded-full"></button>
-        <button className="h-3 w-3 bg-brand-200 rounded-full"></button>
+      <div className="flex gap-2 justify-center mt-10">
+        <button onClick={click1} className="h-3 w-3 bg-brand-500 rounded-full"></button>
+        <button onClick={click2} className="h-3 w-3 bg-brand-100 rounded-full"></button>
       </div>
     </section>
   );
